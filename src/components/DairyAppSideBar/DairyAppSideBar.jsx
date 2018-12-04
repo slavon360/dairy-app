5c408bc2ef2111b5e2ca7b9c06efee1f8bcdeb69
viacheslav liakh <slyakh@verasoftlabs.eu>
2018-12-04 20:55:35 +0200

    beginning

---
+++ src/components/DairyAppSideBar/DairyAppSideBar.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const DairyAppSidebar = () => (
+    <div>
+        <h2>Dairy App</h2>
+        <div>Comment with no sense</div>
+    </div>
+);
+
+export default DairyAppSidebar;
